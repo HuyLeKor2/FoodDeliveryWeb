3d48b68a71817f21ba86765d1663a3ddb2658a08
Minh Huy <huylekore.071103@gmail.com>
2024-11-18 12:17:38 +0700

    Hoàn thành login/logout: pages và components và logic xử lý ( routes, đường dẫn và data)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,14 +4,38 @@ import { ThemeProvider } from '@emotion/react';
 import darkTheme from './theme/DarkTheme';
 import { CssBaseline } from '@mui/material';
 import './index.css';
-import { Navbar } from './customers/component/Navbar/Navbar';
-import { Home } from './customers/component/Home/Home';
+import HomePage from './customers/pages/Home/HomePage';
+import Navbar from './customers/components/Navbar/Navbar';
+import CustomerRoutes from './Routers/CustomerRoutes';
+import { getAllRestaurantsAction, getRestaurantByUserId } from './State/Customers/Restaurant/restaurant.action';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getUser } from './State/Authentication/Action';
 function App() {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+  const jwt = localStorage.getItem("jwt");
+
+  useEffect(() => {
+
+    if (jwt) {
+      dispatch(getUser(jwt));
+      // dispatch(findCart(jwt));
+      // dispatch(getAllRestaurantsAction(jwt));
+    }
+  }, [auth.jwt]);
+
+  // useEffect(() => {
+  //   if (auth.user?.role == "ROLE_RESTAURANT_OWNER") {
+  //     dispatch(getRestaurantByUserId(auth.jwt || jwt));
+  //   }
+  // }, [auth.user]);
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Navbar />
-      <Home />
+      {/* <Navbar /> */}
+      {/* <HomePage /> */}
+      <CustomerRoutes />
     </ThemeProvider>
   );
 }
